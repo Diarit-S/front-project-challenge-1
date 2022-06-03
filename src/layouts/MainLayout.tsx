c4869a3496e4b92f2643adfdbdce5134da1608ff
Diarit Salihaj <diarit-94@hotmail.fr>
2022-06-03 11:44:18 +0000

    GP-8 Manage the main layout, add a header and some style

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,23 @@
 import { Outlet } from 'react-router-dom'
 
+import ResponsiveAppBar from 'components/ResponsiveAppBar'
+
 import React from 'react'
+import { Box } from '@mui/material'
 
 const MainLayout = () => {
   return (
     <main>
-      <Outlet />
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Box
+        sx={{
+          backgroundColor: '#323259',
+          padding: '20px',
+          marginTop: '50px',
+          width: 'fit-content'
+        }}>
+        <Outlet />
+      </Box>
     </main>
   )
 }
